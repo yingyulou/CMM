0060745d4dd1be786351045e5816400c21466170
yingyulou <yingyulou@vip.qq.com>
2021-01-15 09:46:32 +0800

    ~

--- src/TokenType.hpp
+++ src/TokenType.hpp
@@ -17,60 +17,60 @@ namespace CMM
 enum class TOKEN_TYPE
 {
     // Word
-    ID,                   // ID
-    NUMBER,               // Number
+    ID,                    // ID
+    NUMBER,                // Number
 
     // Keyword
-    VOID,                 // void
-    INT,                  // int
-    IF,                   // if
-    ELSE,                 // else
-    WHILE,                // while
-    RETURN,               // return
+    VOID,                  // void
+    INT,                   // int
+    IF,                    // if
+    ELSE,                  // else
+    WHILE,                 // while
+    RETURN,                // return
 
     // Operator
-    PLUS,                 // +
-    MINUS,                // -
-    MULTIPLY,             // *
-    DIVIDE,               // /
-    LESS,                 // <
-    LESS_EQUAL,           // <=
-    GREATER,              // >
-    GREATER_EQUAL,        // >=
-    EQUAL,                // ==
-    NOT_EQUAL,            // !=
-    ASSIGN,               // =
-    SEMICOLON,            // ;
-    COMMA,                // ,
-    LEFT_ROUND_BRACKET,   // (
-    RIGHT_ROUND_BRACKET,  // )
-    LEFT_SQUARE_BRACKET,  // [
-    RIGHT_SQUARE_BRACKET, // ]
-    LEFT_CURLY_BRACKET,   // {
-    RIGHT_CURLY_BRACKET,  // }
+    PLUS,                  // +
+    MINUS,                 // -
+    MULTIPLY,              // *
+    DIVIDE,                // /
+    LESS,                  // <
+    LESS_EQUAL,            // <=
+    GREATER,               // >
+    GREATER_EQUAL,         // >=
+    EQUAL,                 // ==
+    NOT_EQUAL,             // !=
+    ASSIGN,                // =
+    SEMICOLON,             // ;
+    COMMA,                 // ,
+    LEFT_ROUND_BRACKET,    // (
+    RIGHT_ROUND_BRACKET,   // )
+    LEFT_SQUARE_BRACKET,   // [
+    RIGHT_SQUARE_BRACKET,  // ]
+    LEFT_CURLY_BRACKET,    // {
+    RIGHT_CURLY_BRACKET,   // }
 
     // EOF
-    END_OF_FILE,          // EOF
+    END_OF_FILE,           // EOF
 
     // AST
-    DECL_LIST,            // AST: DeclList
-    VAR_DECL,             // AST: VarDecl
-    FUNC_DECL,            // AST: FuncDecl
-    PARAM_LIST,           // AST: ParamList
-    PARAM,                // AST: Param
-    COMPOUND_STMT,        // AST: CompoundStmt
-    LOCAL_DECL,           // AST: LocalDecl
-    STMT_LIST,            // AST: StmtList
-    IF_STMT,              // AST: IfStmt
-    WHILE_STMT,           // AST: WhileStmt
-    RETURN_STMT,          // AST: ReturnStmt
-    EXPR,                 // AST: Expr
-    VAR,                  // AST: Var
-    SIMPLE_EXPR,          // AST: SimpleExpr
-    ADD_EXPR,             // AST: AddExpr
-    TERM,                 // AST: Term
-    CALL,                 // AST: Call
-    ARG_LIST,             // AST: ArgList
+    DECL_LIST,             // AST: DeclList
+    VAR_DECL,              // AST: VarDecl
+    FUNC_DECL,             // AST: FuncDecl
+    PARAM_LIST,            // AST: ParamList
+    PARAM,                 // AST: Param
+    COMPOUND_STMT,         // AST: CompoundStmt
+    LOCAL_DECL,            // AST: LocalDecl
+    STMT_LIST,             // AST: StmtList
+    IF_STMT,               // AST: IfStmt
+    WHILE_STMT,            // AST: WhileStmt
+    RETURN_STMT,           // AST: ReturnStmt
+    EXPR,                  // AST: Expr
+    VAR,                   // AST: Var
+    SIMPLE_EXPR,           // AST: SimpleExpr
+    ADD_EXPR,              // AST: AddExpr
+    TERM,                  // AST: Term
+    CALL,                  // AST: Call
+    ARG_LIST,              // AST: ArgList
 };
 
 
